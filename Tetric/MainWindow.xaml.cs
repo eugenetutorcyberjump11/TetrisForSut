9b44e786fad47b32047404559323a1596af72feb
demenko <eugenetutor@gmail.com>
2024-06-15 18:29:09 +0300

    Add project files.

---
+++ Tetric/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Tetric
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Game _game;
+        private DispatcherTimer _timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _game = new Game(GameCanvas);
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(500);
+            _timer.Tick += GameTick;
+
+            this.KeyDown += MainWindow_KeyDown;     
+        }
+
+        private void GameTick(object sender, EventArgs e)
+        {
+            _game.MoveDown();
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (_game == null) return;
+
+            switch (e.Key) {
+                case Key.Left:
+                    _game.MoveLeft();
+                    break;
+                case Key.Right:
+                    _game.MoveRight();
+                    break;
+                case Key.Up:
+                    _game.Rotate();
+                    break;
+                case Key.Down:
+                    _game.MoveDown();
+                    break;
+            }
+
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            _game.StartNewGame();
+            _timer.Start();
+        }
+    }
+}
---
+++ Tetric/Tetromino.cs
@@ -0,0 +1,94 @@
+﻿using System.Windows.Media;
+using System.Windows;
+using System.Collections.Generic;
+
+namespace Tetric
+{
+    public enum TetromineType
+    {
+        I, O, T, S, Z, J, L
+    }
+
+    public class Tetromino
+    {
+        public TetromineType Type { get; private set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public List<Point> Blocks { get; private set; }
+        public Brush Color { get; private set; }
+
+        public Tetromino(TetromineType type, int x, int y)
+        {
+            Type = type;
+            X = x;
+            Y = y;
+            InitializeBlocks();
+        }
+
+        private void InitializeBlocks()
+        {
+            Blocks = new List<Point>();
+            switch(Type)
+            {
+                case TetromineType.I:
+                    Blocks.Add(new Point(0, 0));
+                    Blocks.Add(new Point(1, 0));
+                    Blocks.Add(new Point(2, 0));
+                    Blocks.Add(new Point(3, 0));
+                    Color = Brushes.Cyan;
+                    break;
+                case TetromineType.O:
+                    Blocks.Add(new Point(0, 0));
+                    Blocks.Add(new Point(1, 0));
+                    Blocks.Add(new Point(0, 1));
+                    Blocks.Add(new Point(1, 1));
+                    Color = Brushes.Yellow;
+                    break;
+                case TetromineType.T:
+                    Blocks.Add(new Point(1, 0));
+                    Blocks.Add(new Point(0, 1));
+                    Blocks.Add(new Point(1, 1));
+                    Blocks.Add(new Point(2, 1));
+                    Color = Brushes.Purple;
+                    break;
+                case TetromineType.S:
+                    Blocks.Add(new Point(1, 0));
+                    Blocks.Add(new Point(2, 0));
+                    Blocks.Add(new Point(0, 1));
+                    Blocks.Add(new Point(1, 1));
+                    Color = Brushes.Green;
+                    break;
+                case TetromineType.Z:
+                    Blocks.Add(new Point(0, 0));
+                    Blocks.Add(new Point(1, 0));
+                    Blocks.Add(new Point(1, 1));
+                    Blocks.Add(new Point(2, 1));
+                    Color = Brushes.Red;
+                    break;
+                case TetromineType.J:
+                    Blocks.Add(new Point(0, 0));
+                    Blocks.Add(new Point(0, 1));
+                    Blocks.Add(new Point(1, 1));
+                    Blocks.Add(new Point(2, 1));
+                    Color = Brushes.Blue;
+                    break;
+                case TetromineType.L:
+                    Blocks.Add(new Point(2, 0));
+                    Blocks.Add(new Point(0, 1));
+                    Blocks.Add(new Point(1, 1));
+                    Blocks.Add(new Point(2, 1));
+                    Color = Brushes.Orange;
+                    break;
+            }
+        }
+
+        public void Rotate()
+        {
+            for (int i = 0; i < Blocks.Count; i++)
+            {
+                double x = Blocks[i].X;
+                Blocks[i] = new Point(Blocks[i].Y, -x);
+            }
+        }
+    }
+}
